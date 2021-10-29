03eb1b057bb29b2c17c9f6884ef93e846a7493e8
Brithany <brithany.tigre.7e4@itb.cat>
2021-10-29 15:30:38 +0200

    initial commit

---
+++ src/main/java/cat/itb/projecte1/api/checkme_api/CheckMeApiApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.projecte1.api.checkme_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckMeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/itb/projecte1/api/checkme_api/CheckMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.projecte1.api.checkme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
