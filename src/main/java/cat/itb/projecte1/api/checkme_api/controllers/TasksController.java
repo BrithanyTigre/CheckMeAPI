f0bca360a006aef71fba843ec3935c20a7d2dbf5
Brithany <brithany.tigre.7e4@itb.cat>
2021-11-02 16:08:48 +0100

    controllers done

--- src/main/java/cat/itb/projecte1/api/checkme_api/controllers/TasksController.java
+++ src/main/java/cat/itb/projecte1/api/checkme_api/controllers/TasksController.java
@@ -1,13 +1,53 @@
 package cat.itb.projecte1.api.checkme_api.controllers;
 
+import cat.itb.projecte1.api.checkme_api.model.entities.Task;
 import cat.itb.projecte1.api.checkme_api.model.services.TasksService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TasksController {
     private final TasksService tasksService;
 
+    @GetMapping("/todoitems")
+    public List<Task> listTasks(){
+        return tasksService.listTasks();
+    }
 
+    @GetMapping("/todoitems/{id}")
+    public ResponseEntity<?> getTask(@PathVariable String id){
+        Task res = tasksService.getTask(id);
+        if (res == null){
+            return ResponseEntity.notFound().build();
+        }else{
+            return ResponseEntity.ok(res);
+        }
+    }
+
+    @PostMapping("/todoitems")
+    public ResponseEntity<?> addTask(@RequestBody Task task){
+        Task res = tasksService.addTask(task);
+        return new ResponseEntity<Task>(res, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/todoitems/{id}")
+    public ResponseEntity<?> modifyTask(@RequestBody Task task){
+        Task res = tasksService.modifyTask(task);
+        if (res == null){
+            return ResponseEntity.notFound().build();
+        }else{
+            return ResponseEntity.ok(res);
+        }
+    }
+
+    @DeleteMapping("todoitems/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable String id){
+        Task res = tasksService.deleteTask(id);
+        return new ResponseEntity<Task>(res, HttpStatus.NO_CONTENT);
+    }
 }
