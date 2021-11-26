57e318d885326f631fcb50b60178fec3640af095
Brithany <brithany.tigre.7e4@itb.cat>
2021-11-26 15:17:53 +0100

    CORS

--- src/main/java/cat/itb/projecte1/api/checkme_api/CheckMeApiApplication.java
+++ src/main/java/cat/itb/projecte1/api/checkme_api/CheckMeApiApplication.java
@@ -2,12 +2,28 @@ package cat.itb.projecte1.api.checkme_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CheckMeApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckMeApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/todolists/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+        };
     }
 
 }
