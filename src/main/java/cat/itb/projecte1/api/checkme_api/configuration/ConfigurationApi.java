519a535118967ef58bd57d092d555a875df61f82
Brithany <brithany.tigre.7e4@itb.cat>
2021-11-30 15:28:00 +0100

    CORS

--- src/main/java/cat/itb/projecte1/api/checkme_api/configuration/ConfigurationApi.java
+++ src/main/java/cat/itb/projecte1/api/checkme_api/configuration/ConfigurationApi.java
@@ -1,10 +1,12 @@
 package cat.itb.projecte1.api.checkme_api.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class Configuration {
+@Configuration
+public class ConfigurationApi {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
